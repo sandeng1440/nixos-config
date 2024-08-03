5e61e64bee952211cae8d7535f694979c3819aef
hdengsan <sanhenden@gmail.com>
2024-08-03 21:08:56 +0300

    Install script: commencement of development

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
+# Development switch
+echo "Install script is incomplete. Exiting"
+exit
+
+## Flake installation
 cd || exit
 if command -v git &> /dev/null; then
 	git clone https://github.com/sandeng1440/nixos-config.git
 fi
 cd nixos-config || exit
 
+## Hyde-cli installation
+
+## Hyprdots installation
+
+## Theme patcher
