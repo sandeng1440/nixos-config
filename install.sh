eb41ed74b80a50f85d8a43ec1a07708c48a0229b
hdengsan <sanhenden@gmail.com>
2024-08-04 18:09:42 +0300

    install script complete

--- install.sh
+++ install.sh
@@ -1,31 +1,46 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p git gnumake nano
+
 # Development switch
-read -rp "Install script is incomplete. Exit? [Y/n] " confirm
-if [ -z "$confirm" ]; then 
-	exit
-fi
-if [[ $confirm == "y" || $confirm == "Y" ]]; then 
-	exit
-fi
+# read -rp "Install script is incomplete. Exit? [Y/n] " confirm
+# if [ -z "$confirm" ]; then 
+# 	exit
+# fi
+# if [[ $confirm == "y" || $confirm == "Y" ]]; then 
+# 	exit
+# fi
 #
 
 ## Variables
-set -e
 username=$(whoami)
-read -rp "Choose your text editor [ vim ]" editor
-if [ -z "$editor" ]; then
-	editor="vim"
-fi
-if $editor flake.nix; then
+echo "Cloning nixos-config..."
+git clone https://github.com/sandeng1440/nixos-config.git ~/nixos-config || echo "FATAL ERROR: Git closed" && exit
+echo "Opening flake.nix with nano..."
+sleep 2 && nano ~/nixos-config/flake.nix
 
 ## Flake installation
-cd || exit
-if command -v git &> /dev/null; then
-	git clone https://github.com/sandeng1440/nixos-config.git
-fi
-cd nixos-config || exit
+nixos-generate-config --show-hardware-config > ~/nixos-config/host/hardware.nix
+echo "-----------------------------------"
+echo "Rebuilding the nix flake..."
+sudo nixos-rebuild switch --flake ~/nixos-config
 
 ## Hyde-cli installation
+echo "Cloning Hyde-cli..."
+git clone https://github.com/kRHYME7/Hyde-cli.git ~/Hyde-cli || echo "FATAL ERROR: Git closed" && exit
+cd ~/Hyde-cli
+make LOCAL=1
 
 ## Hyprdots installation
+echo "-----------------------------------"
+echo "Cloning hyprdots..."
+git clone https://github.com/prasanthrangan/hyprdots.git --depth=1 ~/hyprdots || echo "FATAL ERROR: Git closed" && exit
+cd ~/hyprdots/Scripts || echo "FATAL ERROR: Could not cd into ~/hyprdots/Scripts" && exit
+sed -i '/continue\ 2/d' ~/hyprdots/Scripts/restore_cfg.sh
 
 ## Theme patcher
+Hyde-install --dir ~/hyprdots --link
+Hyde restore Config
+echo "Patching themes..."
+sleep 2
+Hyde theme patch
+
