7bb721385d459612bc3d5d97835aff58eadfcc34
sandeng1440 <sandeng1440@gmail.com>
2024-09-03 13:57:48 +0300

    swapfile

--- host/hardware.nix
+++ host/hardware.nix
@@ -24,7 +24,10 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 8*1024;
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
