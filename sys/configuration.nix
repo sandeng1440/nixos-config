f205395ca96b28a9d96cbc65e2ddb44a90232f50
sandeng1440 <sandeng1440@gmail.com>
2025-07-25 08:38:55 +0300

    networking fix

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -29,7 +29,6 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   networking.hostName = "${hostname}";
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
