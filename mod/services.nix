f1acb5d53b0500d491889f9baa64440f64a25009
sandeng1440 <sandeng1440@gmail.com>
2025-07-25 10:00:16 +0300

    flatpaks

--- mod/services.nix
+++ mod/services.nix
@@ -11,12 +11,12 @@
   services.power-profiles-daemon.enable = true;
   services.flatpak = {
     enable = true;
-    extraRepositories = [
-      {
-        name = "flathub";
-        url = "https://flathub.org/repo/flathub.flatpakrepo";
-      }
-    ];
+    # extraRepositories = [
+    #   {
+    #     name = "flathub";
+    #     url = "https://flathub.org/repo/flathub.flatpakrepo";
+    #   }
+    # ];
   };
 
   services.pipewire = {
