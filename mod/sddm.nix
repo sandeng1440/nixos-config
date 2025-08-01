c2632d949f7cd3f0af8861d1f8415d22406e7013
sandeng1440 <sandeng1440@gmail.com>
2025-08-01 21:55:58 +0300

    sddm: astronaut-theme

--- mod/sddm.nix
+++ mod/sddm.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   services.displayManager.sddm = {
     enable = true;
+    wayland.enable = true;
     theme = "sddm-astronaut-theme";
   };
   environment.systemPackages = with pkgs; [
