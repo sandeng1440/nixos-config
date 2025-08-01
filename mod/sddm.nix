6f37bd15f4abd1bbff225aa20a871849453aae88
sandeng1440 <sandeng1440@gmail.com>
2025-08-01 20:12:26 +0300

    sddm: astronaut-theme

--- mod/sddm.nix
+++ mod/sddm.nix
@@ -2,8 +2,7 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "Graphite";
-    package = pkgs.libsForQt5.sddm;
+    theme = "sddm-astronaut-theme";
   };
   environment.systemPackages = with pkgs; [
     kdePackages.qtmultimedia
@@ -12,13 +11,11 @@
     kdePackages.qt5compat
     libsForQt5.qt5.qtgraphicaleffects
     libsForQt5.qt5.qtquickcontrols2
-    sddm-chili-theme
-    graphite-kde-theme
-    # sddm-astronaut
-    # (pkgs.where-is-my-sddm-theme.override {
-    #   themeConfig.General = {
-    #     background = (builtins.toString ./sddm-background.png);
-    #   };
-    # })
+
+    (pkgs.sddm-astronaut.override {
+      themeConfig.General = {
+        embeddedTheme = "post-apocalyptic_hacker";
+      };
+    })
   ];
 }
