afd541d39e267f78dd1c1bd45b5fee294d2f8ef6
sandeng1440 <sandeng1440@gmail.com>
2025-08-02 15:00:27 +0300

    sddm: elegant-sddm theme

--- mod/sddm.nix
+++ mod/sddm.nix
@@ -2,21 +2,19 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "where_is_my_sddm_theme";
+    autoNumlock = true;
+    theme = "Elegant";
+    package = pkgs.kdePackages.sddm;
+    extraPackages = [
+      kdePackages.qtmultimedia
+      kdePackages.qtvirtualkeyboard
+      kdePackages.qtsvg
+      kdePackages.qt5compat
+      libsForQt5.qt5.qtgraphicaleffects
+      libsForQt5.qt5.qtquickcontrols2
+      elegant-sddm
+    ];
   };
   environment.systemPackages = with pkgs; [
-    kdePackages.qtmultimedia
-    kdePackages.qtvirtualkeyboard
-    kdePackages.qtsvg
-    kdePackages.qt5compat
-    libsForQt5.qt5.qtgraphicaleffects
-    libsForQt5.qt5.qtquickcontrols2
-    elegant-sddm
-    where-is-my-sddm-theme
-
-    # sddm-astronaut
-    # (pkgs.sddm-astronaut.override {
-    #   embeddedTheme = "black_hole";
-    # })
   ];
 }
