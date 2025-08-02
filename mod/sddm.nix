bfcf7ea7aea8d5e676b5302f086f824e3e4a06d3
sandeng1440 <sandeng1440@gmail.com>
2025-08-02 15:05:37 +0300

    sddm: elegant-sddm theme

--- mod/sddm.nix
+++ mod/sddm.nix
@@ -5,16 +5,14 @@
     autoNumlock = true;
     theme = "Elegant";
     package = pkgs.kdePackages.sddm;
-    extraPackages = with pkgs; [
-      kdePackages.qtmultimedia
-      kdePackages.qtvirtualkeyboard
-      kdePackages.qtsvg
-      kdePackages.qt5compat
-      libsForQt5.qt5.qtgraphicaleffects
-      libsForQt5.qt5.qtquickcontrols2
-      elegant-sddm
-    ];
   };
   environment.systemPackages = with pkgs; [
+    kdePackages.qtmultimedia
+    kdePackages.qtvirtualkeyboard
+    kdePackages.qtsvg
+    kdePackages.qt5compat
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtquickcontrols2
+    elegant-sddm
   ];
 }
