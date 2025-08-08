f06be0eed12931e5f17fc298aba71ed5496cf925
sandeng1440 <sandeng1440@gmail.com>
2025-08-08 18:31:16 +0300

    display-manager: sddm

--- mod/display-manager.nix
+++ mod/display-manager.nix
@@ -3,12 +3,12 @@
     enable = true;
     xkb.layout = "gb";
   };
-  services.xserver.displayManager.sddm = {
+  services.displayManager.sddm = {
     enable = true;
     theme = "chili";
   };
   environment.systemPackages = with pkgs; [
-    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtquickcontrols
     sddm-chili-theme
   ];
 }
