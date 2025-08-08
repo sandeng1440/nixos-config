31fa83631e0356e82cd335aa9ffa50dc23fbc12f
sandeng1440 <sandeng1440@gmail.com>
2025-08-08 16:34:01 +0300

    display-manager: sddm

--- mod/display-manager.nix
+++ mod/display-manager.nix
@@ -1,33 +1,33 @@
 { pkgs, ... }: {
   # LightDM
-  services.xserver = {
-    enable = true;
-    layout = "uk";
-  };
-  services.xserver.displayManager = {
-    defaultSession = "niri";
-    lightdm = {
-      enable = true;
-      greeters.slick.enable = true;
-    };
-  };
+  # services.xserver = {
+  #   enable = true;
+  #   xkb.layout = "gb";
+  # };
+  # services.displayManager.defaultSession = "niri";
+  # services.xserver.displayManager = {
+  #   lightdm = {
+  #     enable = true;
+  #     greeters.slick.enable = true;
+  #   };
+  # };
 
   # SDDM
-  # services.displayManager.sddm = {
-  #   enable = true;
-  #   wayland.enable = true;
-  #   wayland.compositor = "weston";
-  #   autoNumlock = true;
-  #   theme = "Elegant";
-  #   package = pkgs.kdePackages.sddm;
-  # };
-  # environment.systemPackages = with pkgs; [
-  #   kdePackages.qtmultimedia
-  #   kdePackages.qtvirtualkeyboard
-  #   kdePackages.qtsvg
-  #   kdePackages.qt5compat
-  #   libsForQt5.qt5.qtgraphicaleffects
-  #   libsForQt5.qt5.qtquickcontrols2
-  #   elegant-sddm
-  # ];
+  services.displayManager.sddm = {
+    enable = true;
+    # wayland.enable = true;
+    # wayland.compositor = "weston";
+    autoNumlock = true;
+    theme = "Elegant";
+    package = pkgs.kdePackages.sddm;
+  };
+  environment.systemPackages = with pkgs; [
+    kdePackages.qtmultimedia
+    kdePackages.qtvirtualkeyboard
+    kdePackages.qtsvg
+    kdePackages.qt5compat
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtquickcontrols2
+    elegant-sddm
+  ];
 }
