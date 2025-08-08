709e49f6352f16f007a650c6f1289107d6e776e6
sandeng1440 <sandeng1440@gmail.com>
2025-08-08 16:50:38 +0300

    display-manager: sddm

--- mod/display-manager.nix
+++ mod/display-manager.nix
@@ -16,18 +16,18 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    # wayland.compositor = "weston";
+    wayland.compositor = "weston";
     autoNumlock = true;
     theme = "Elegant";
-    package = pkgs.kdePackages.sddm;
+    package.name = pkgs.kdePackages.sddm;
   };
   environment.systemPackages = with pkgs; [
-    kdePackages.qtmultimedia
-    kdePackages.qtvirtualkeyboard
-    kdePackages.qtsvg
-    kdePackages.qt5compat
-    libsForQt5.qt5.qtgraphicaleffects
-    libsForQt5.qt5.qtquickcontrols2
+    # kdePackages.qtmultimedia
+    # kdePackages.qtvirtualkeyboard
+    # kdePackages.qtsvg
+    # kdePackages.qt5compat
+    # libsForQt5.qt5.qtgraphicaleffects
+    # libsForQt5.qt5.qtquickcontrols2
     elegant-sddm
   ];
 }
